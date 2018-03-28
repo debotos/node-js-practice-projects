5ac99ea98fe5075c7175d837d3c0c9c6c55e13a4
debotos <debotosdas@gmail.com>
2018-03-28 16:37:59 +0600

    added deployment configuration

--- node_authentication/config/keys/production.js
+++ node_authentication/config/keys/production.js
@@ -1,1 +1,30 @@
-module.exports = {};
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  facebookAuth: {
+    clientID: process.env.FACEBOOK_CLIENT_ID, // App ID
+    clientSecret: process.env.FACEBOOK_CLIENT_SECRET, // App Sectet
+    callbackURL: process.env.FACEBOOK_CALLBACK_URL,
+    profileURL: 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
+    profileFields: ['id', 'displayName', 'photos', 'email'] // Asking for this data
+  },
+  twitterAuth: {
+    consumerKey: process.env.TWITTER_CONSUMER_KEY,
+    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+    callbackURL: process.env.TWITTER_CALLBACK_URL
+  },
+  googleAuth: {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
+  },
+  githubAuth: {
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.GITHUB_CALLBACK_URL
+  },
+  linkedinAuth: {
+    clientID: process.env.LINKEDIN_CLIENT_ID,
+    clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+    callbackURL: process.env.LINKEDIN_CALLBACK_URL
+  }
+};
